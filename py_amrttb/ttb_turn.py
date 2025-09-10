e05fc47ca489e26ad602fbecaa444f2f15f4e9a5
wangrui04 <wangruielena@gmail.com>
2025-09-10 10:47:57 -0400

    we're about to change robots

--- py_amrttb/ttb_turn.py
+++ py_amrttb/ttb_turn.py
@@ -45,7 +45,7 @@ class TTBController(Node):
 
         self.subscriber_ = self.create_subscription(
             IrIntensityVector,
-            '/TTB08/ir_itensity',
+            '/TTB08/ir_intensity',
             self.ir_callback,
             qos_profile
 
@@ -57,13 +57,31 @@ class TTBController(Node):
 
         # when count >=30, stop moving vehicle
         self.count = 0
+
+        #step1
         self.forward = 0
         self.angular = 0
 
-        self.distance = np.inf
+        #step 2
+        self.distance = 0
         self.turn = False
         self.turnDirection = 0
         self.random = 0
+        self.time = 0
+
+        #step3
+        self.previous_error = 0
+        self.integral = 0
+        self.setpoint_two = 0.2
+        self.setpoint_four = 0.4
+        self.output = 0
+        self.measured_value = 0
+        self.count_three = 0
+        self.dt = 5
+        self.kp = 0.1
+        self.ki = 0.001
+        self.kd = 0.03
+
 
     def controller_callback(self):
         # create msg which makes TTB speed 0.1 m/s and angular velocity 0.4rad/s
@@ -71,8 +89,8 @@ class TTBController(Node):
 
         msg = Twist()
         #if self.count < 30:
-        #    msg.linear.x  = 0.1
-        #    msg.angular.z = 0.4
+         #   msg.linear.x  = 0.1
+          #  msg.angular.z = 0.4
             
         #step 1 
         if self.forward !=0:
@@ -89,9 +107,9 @@ class TTBController(Node):
 
         #step 2
 
-        if self.distance >= 20:
+        if self.distance >= 50:
             self.turn = True
-            self.count = 0
+            self.time = 0
         else:
             self.turn = False
             self.random = random.uniform(-1,1)
@@ -99,18 +117,32 @@ class TTBController(Node):
                 self.turnDirection = 1
             else:
                 self.turnDirection = -1
-
-
         
-        if self.count < 30 & self.turn == True:
+        if self.time < 30 and self.turn == True:
             msg.angular.z = self.turnDirection * 5
-            count += 1
+            self.time += 1
 
   
-            
 
-        # self.publisher_.publish(msg)
-        # self.count += 1
+        #step3
+        # msg.linear.x = 0.1
+
+        # if self.count_three ==0 :
+        #     self.count_three = self.dt
+        #     error = self.setpoint_two - self.measured_value
+        #     self.integral += self.error*(self.dt/10)
+        #     self.derivative = (self.error - self.previous_error)/(self.dt/10)
+        #     self.output += self.kp*self.error + self.ki*self.integral + self.kd*self.derivative
+        #     self.previous_error = error
+        
+        # self.count_three -= 1
+        
+
+
+
+
+        self.publisher_.publish(msg)
+        self.count += 1
 
     def imu_callback(self, msg):
         # print angular velocity from imu message to console
@@ -131,11 +163,16 @@ class TTBController(Node):
     #step 2
     def ir_callback(self, msg):
         # print angular velocity from imu message to console
-        self.distance = msg.readings[3].value + msg.readings[4].value
+        self.distance = (msg.readings[3].value + msg.readings[4].value + msg.readings[2].value + msg.readings[5].value)*0.25
         
         self.get_logger().info(f'ir_readings: {self.distance:0.10f}')
 
-    
+    '''
+    #step 3
+    def odom_callback(self, msg):
+        self.
+
+    '''
 
 def main(args=None):
     rclpy.init(args=args)
